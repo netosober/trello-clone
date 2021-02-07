596b78d9bee31e1de989aa55017ab5c2223c22d2
Ernesto Soberanes <ernesto@toptal.com>
2021-02-07 12:31:16 -0800

    Add Focus and Enter handling

--- src/NewItemForm.tsx
+++ src/NewItemForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useFocus } from './utils/useFocus'
 import { NewItemFormContainer, NewItemButton, NewItemInput } from './styles'
 
 interface NewItemFormProps {
@@ -7,12 +8,22 @@ interface NewItemFormProps {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState('');
+  const inputRef = useFocus()
+  const handleAddText = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === 'Enter') {
+      onAdd(text)
+    }
+  }
 
   return (
     <NewItemFormContainer>
       <NewItemInput
+        ref={inputRef}
         value={text}
         onChange={e => setText(e.target.value)}
+        onKeyPress={handleAddText}
       />
       <NewItemButton onClick={() => onAdd(text)}>
         Create
