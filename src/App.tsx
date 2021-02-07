f272c24533109c632e7977870af27ef3ac0d8c47
Ernesto Soberanes <ernesto@toptal.com>
2021-02-07 09:11:57 -0800

    Add new item forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Column } from './Column'
+import { Card } from './Card'
+import { AddNewItem } from './AddNewItem'
 import { AppContainer } from "./styles"
 
 const App = () => {
   return (
     <AppContainer>
-    Columns will go here
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
   )
 }
